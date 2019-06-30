6748bf7721f43c7a1188cc48ee1bfd74b357e2f3
mjbotes <50694994+mjbotes@users.noreply.github.com>
2019-06-30 22:20:29 +0200

    READY FOR 2D Game

--- Assets/ChairScript.cs
+++ Assets/ChairScript.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChairScript : MonoBehaviour
 {
     public GameObject uiObject;
+    public KeyCode interactKey;
+    public string computerGame;
+
     void Start()
     {
         uiObject.SetActive(false);
     }
 
-    void onTriggerEnter (Collider player)
+    void OnTriggerEnter (Collider player)
     {
         if (player.gameObject.tag == "Player")
         {
@@ -18,11 +22,19 @@ public class ChairScript : MonoBehaviour
         }
     }
 
-    //void onTriggerExit (Collider player)
-    //{
-     //   if (player.gameObject.tag == "Player")
-     //   {
-     //       uiObject.SetActive(false);
-     //   }
-   // }
+    void OnTriggerStay (Collider player)
+    {
+        if (Input.GetKeyDown(interactKey) && player.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(computerGame);
+        }
+    }
+
+    void OnTriggerExit (Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(false);
+        }
+    }
 }
