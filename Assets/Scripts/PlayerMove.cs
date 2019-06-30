6e7ff70afd8d908818d06cb2a1bdb421f2b1cafe
mjbotes <50694994+mjbotes@users.noreply.github.com>
2019-06-30 11:24:58 +0200

    FPC_ADDED

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private KeyCode sprintKey;
+
+
+    private CharacterController characterController;
+
+    private bool isSprinting;
+    private bool isJumping;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float verticalInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * verticalInput;
+        Vector3 rightMovement = transform.right * horizontalInput;
+
+        characterController.SimpleMove(forwardMovement + rightMovement);
+
+        SprintInput(verticalInput);
+        JumpInput();
+    }
+
+    private void SprintInput(float verticalInput)
+    {
+        if (Input.GetKeyDown(sprintKey) && !isSprinting && verticalInput > 0.0f)
+        {
+            isSprinting = true;
+            movementSpeed *= 2;
+        }
+
+        if (Input.GetKeyUp(sprintKey) && isSprinting)
+        {
+            isSprinting = false;
+            movementSpeed /= 2;
+        }
+    }
+
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        characterController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            characterController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        }
+        while (!characterController.isGrounded && characterController.collisionFlags != CollisionFlags.Above);
+
+        characterController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
