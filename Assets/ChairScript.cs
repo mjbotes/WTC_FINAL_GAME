981cfe3d6658c3c9b10e4453a81101ad5bbb01d6
mjbotes <50694994+mjbotes@users.noreply.github.com>
2019-06-30 21:10:43 +0200

    OBJECT COLLIDER

---
+++ Assets/ChairScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairScript : MonoBehaviour
+{
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void onTriggerEnter (Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+        }
+    }
+
+    //void onTriggerExit (Collider player)
+    //{
+     //   if (player.gameObject.tag == "Player")
+     //   {
+     //       uiObject.SetActive(false);
+     //   }
+   // }
+}
